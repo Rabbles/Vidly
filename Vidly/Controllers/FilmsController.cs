e16f535559a256ccf0f1cb679bf44b25152a2c27
Rab Horner <rab_horner@outlook.com>
2021-03-19 09:50:00 +0000

    Add project files.

---
+++ Vidly/Controllers/FilmsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class FilmsController : Controller
+    {
+        // GET: Films
+        public ActionResult Random()
+        {
+            var film = new Models.Film { Name = "A View To A Kill" };
+
+            return View(film);
+        }
+    }
+}
