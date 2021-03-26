00182fb2e4a3c539611c916897321f6c361b9427
Rab Horner <rab_horner@outlook.com>
2021-03-26 10:49:25 +0000

    database context set and migrations enabled

---
+++ Vidly/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Vidly.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+    }
+}
