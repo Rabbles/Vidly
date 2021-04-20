25107122fc71fc7cabf562eba2efb41786171f07
Rab Horner <rab_horner@outlook.com>
2021-04-20 20:31:58 +0100

    Migrations

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,5 +5,12 @@
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
     }
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,14 @@
+﻿namespace Vidly.Models
+{
+    public class MembershipType
+    {
+      
+        public byte Id { get; set; }
+        public decimal SignUpFee { get; set; }
+
+        public byte DurationInMonths { get; set; }
+
+        public byte DiscountRate { get; set;}
+
+    }
+}
